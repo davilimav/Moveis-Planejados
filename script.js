7bc8454ab69f353ad1c510c3ce37fd5ef386847d
Davi <vergueirolimadavi@gmail.com>
2026-07-16 19:45:46 -0300

    Atualizando responsividade e servidor

--- script.js
+++ script.js
@@ -1,115 +1,267 @@
 function scrollToSection(id) {
+
   document.getElementById(id).scrollIntoView({
-    behavior: "smooth"
+
+    behavior:"smooth"
+
   });
+
 }
 
-document.querySelectorAll(".opcoes").forEach(grupo => {
+
+
+
+
+document.querySelectorAll(".opcoes").forEach(grupo=>{
+
 
   const botoes = grupo.querySelectorAll(".opcao");
 
-  botoes.forEach(botao => {
 
-    botao.addEventListener("click", () => {
+  botoes.forEach(botao=>{
 
-      botoes.forEach(b => b.classList.remove("ativa"));
+
+    botao.addEventListener("click",()=>{
+
+
+      botoes.forEach(b=>{
+
+        b.classList.remove("ativa");
+
+      });
+
+
 
       botao.classList.add("ativa");
 
+
     });
+
 
   });
 
+
 });
 
-async function enviarMensagem(event) {
 
-  event.preventDefault();
 
-  const nome = document.querySelector("input[name='nome']").value;
-  const telefone = document.querySelector("input[name='telefone']").value;
 
-  const ambiente =
-    document.querySelector("[data-grupo='ambiente'] .ativa")?.innerText || "";
 
-  const estilo =
-    document.querySelector("[data-grupo='estilo'] .ativa")?.innerText || "";
 
-  const cor =
-    document.querySelector("[data-grupo='cor'] .ativa")?.innerText || "";
+async function enviarMensagem(event){
 
-  const investimento =
-    document.querySelector("[data-grupo='investimento'] .ativa")?.innerText || "";
 
-  const prazo =
-    document.querySelector("[data-grupo='prazo'] .ativa")?.innerText || "";
+event.preventDefault();
 
-  try {
 
-    const resposta = await fetch("http://localhost:3000/enviar", {
 
-      method: "POST",
+const nome =
+document.querySelector("input[name='nome']").value;
 
-      headers: {
-        "Content-Type": "application/json"
-      },
 
-      body: JSON.stringify({
-        nome,
-        telefone,
-        ambiente,
-        estilo,
-        cor,
-        investimento,
-        prazo
-      })
 
-    });
+const telefone =
+document.querySelector("input[name='telefone']").value;
 
-    const data = await resposta.json();
 
-if (data.ok) {
 
-    alert("Orçamento enviado com sucesso!");
+const ambiente =
+document.querySelector("[data-grupo='ambiente'] .ativa")?.innerText || "";
 
-    setTimeout(() => {
-        location.reload();
-    }, 2000);
 
-} else {
 
-      document.getElementById("msg").innerText =
-        "Erro ao enviar orçamento.";
+const estilo =
+document.querySelector("[data-grupo='estilo'] .ativa")?.innerText || "";
 
-    }
 
-  } catch (erro) {
 
-    console.log(erro);
+const cor =
+document.querySelector("[data-grupo='cor'] .ativa")?.innerText || "";
 
-    document.getElementById("msg").innerText =
-      "Erro ao enviar orçamento.";
 
-  }
+
+const investimento =
+document.querySelector("[data-grupo='investimento'] .ativa")?.innerText ||"";
+
+
+
+const prazo =
+document.querySelector("[data-grupo='prazo'] .ativa")?.innerText || "";
+
+
+
+
+
+try{
+
+
+const resposta = await fetch("http://localhost:3000/enviar",{
+
+
+method:"POST",
+
+
+headers:{
+
+
+"Content-Type":"application/json"
+
+
+},
+
+
+
+body:JSON.stringify({
+
+
+nome,
+
+telefone,
+
+ambiente,
+
+estilo,
+
+cor,
+
+investimento,
+
+prazo
+
+
+})
+
+
+});
+
+
+
+
+const data = await resposta.json();
+
+
+
+
+if(data.ok){
+
+
+alert("Orçamento enviado com sucesso!");
+
+
+
+setTimeout(()=>{
+
+
+location.reload();
+
+
+},2000);
+
+
 
 }
 
-const btnWhatsapp = document.getElementById("btnWhatsapp");
-const menuWhatsapp = document.querySelector(".whatsapp-menu");
 
-btnWhatsapp.addEventListener("click", () => {
-  menuWhatsapp.classList.toggle("ativo");
+
+else{
+
+
+document.getElementById("msg").innerText =
+"Erro ao enviar orçamento.";
+
+
+}
+
+
+
+}catch(erro){
+
+
+console.log(erro);
+
+
+document.getElementById("msg").innerText =
+"Erro ao enviar orçamento.";
+
+
+}
+
+
+
+}
+
+
+
+
+
+const btnWhatsapp =
+document.getElementById("btnWhatsapp");
+
+
+const menuWhatsapp =
+document.querySelector(".whatsapp-menu");
+
+
+
+if(btnWhatsapp){
+
+
+btnWhatsapp.addEventListener("click",()=>{
+
+
+menuWhatsapp.classList.toggle("ativo");
+
+
 });
 
-document.addEventListener("click", (e) => {
-  if (!document.querySelector(".whatsapp-container").contains(e.target)) {
-    menuWhatsapp.classList.remove("ativo");
-  }
+
+}
+
+
+
+
+
+
+document.addEventListener("click",(e)=>{
+
+
+const container =
+document.querySelector(".whatsapp-container");
+
+
+if(container && !container.contains(e.target)){
+
+
+menuWhatsapp.classList.remove("ativo");
+
+
+}
+
+
 });
 
-const menuToggle = document.getElementById("menuToggle");
-const menuArea = document.querySelector(".menu-area");
 
-menuToggle.addEventListener("click", () => {
-  menuArea.classList.toggle("ativo");
+
+
+const menuToggle =
+document.getElementById("menuToggle");
+
+
+const menuArea =
+document.querySelector(".menu-area");
+
+
+
+if(menuToggle){
+
+
+menuToggle.addEventListener("click",()=>{
+
+
+menuArea.classList.toggle("ativo");
+
+
 });
+
+
+}
--- server.js
+++ server.js
@@ -7,19 +7,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 const transporter = nodemailer.createTransport({
+
   service: "gmail",
+
   auth: {
+
     user: "videiramoveis01@gmail.com",
+
     pass: "lytr nxgw empm lbcu"
+
   }
+
 });
+
+
 
 app.post("/enviar", async (req, res) => {
 
+
   console.log("Recebi:", req.body);
 
+
+
   const {
+
     nome,
     telefone,
     ambiente,
@@ -27,54 +40,101 @@ app.post("/enviar", async (req, res) => {
     cor,
     investimento,
     prazo
+
   } = req.body;
+
+
+
 
   try {
 
+
     await transporter.sendMail({
 
-  from: "videiramoveis01@gmail.com",
 
-  to: "videiramoveis01@gmail.com",
+      from: "videiramoveis01@gmail.com",
 
-  subject: "Novo orçamento - Móveis Videira",
 
-  text: `
-NOVO ORÇAMENTO
+      to: "videiramoveis01@gmail.com",
 
-Nome: ${nome}
 
-Telefone: ${telefone}
+      subject: "Novo orçamento - Móveis Videira",
 
-Tipo de Ambiente: ${ambiente}
 
-Estilo Desejado: ${estilo}
 
-Cor Predominante: ${cor}
+      text: `
 
-Faixa de Investimento: ${investimento}
+NOVO ORÇAMENTO - MÓVEIS VIDEIRA
 
-Prazo Desejado: ${prazo}
-  `
 
-});
+Nome:
+${nome}
 
-    res.json({
-      ok: true
+
+Telefone:
+${telefone}
+
+
+Tipo de Ambiente:
+${ambiente}
+
+
+Estilo Desejado:
+${estilo}
+
+
+Cor Predominante:
+${cor}
+
+
+Faixa de Investimento:
+${investimento}
+
+
+Prazo Desejado:
+${prazo}
+
+      `
+
+
     });
 
-  } catch (error) {
+
+
+    res.json({
+
+      ok:true
+
+    });
+
+
+
+  } catch(error){
+
 
     console.log(error);
 
+
     res.status(500).json({
-      ok: false
+
+      ok:false
+
     });
+
 
   }
 
+
 });
 
-app.listen(3000, () => {
+
+
+
+
+app.listen(3000,()=>{
+
+
   console.log("Servidor rodando na porta 3000");
+
+
 });
